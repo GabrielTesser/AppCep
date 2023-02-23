f8063679f88f29193fc6f0873af5d6ae8900e868
Usuario <Usuario@CPD02-M13>
2023-02-23 15:23:04 -0300

    edição do bairroporcidade.xaml.cs

--- AppCep/AppCep/Service/DataService.cs
+++ AppCep/AppCep/Service/DataService.cs
@@ -8,50 +8,70 @@ using System.Threading.Tasks;
 
 namespace AppCep.Service
 {
-    
-        public class DataService
+
+    public class DataService
+    {
+
+
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
         {
+            Endereco end;
 
-
-            public static async Task<Endereco> GetEnderecoByCep(string cep)
+            using (HttpClient client = new HttpClient())
             {
-                Endereco end;
-
-                using (HttpClient client = new HttpClient())
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/endereco/by-cep?cep=" + cep);
+                if (response.IsSuccessStatusCode)
                 {
-                    HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/endereco/by-cep?cep=" + cep);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string json = response.Content.ReadAsStringAsync().Result;
-                        end = JsonConvert.DeserializeObject<Endereco>(json);
-                    }
-                    else
-                        throw new Exception(response.RequestMessage.Content.ToString());
-
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
                 }
-                return end;
-            }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
 
-            public static async Task<List<Bairro>> GetBarrosByCidade(int id_cidade)
+            }
+            return end;
+        }
+
+        public static async Task<List<Bairro>> GetBarrosByCidade(int id_cidade)
+        {
+            List<Bairro> arr_bairros = new List<Bairro>();
+
+            using (HttpClient client = new HttpClient())
             {
-                List<Bairro> arr_bairros = new List<Bairro>();
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/bairro/by-cidade?id_cidade=");
 
-                using (HttpClient client = new HttpClient())
+                if (response.IsSuccessStatusCode)
                 {
-                    HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/bairro/by-cidade?id_cidade=");
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string json = response.Content.ReadAsStringAsync().Result;
-                        arr_bairros = JsonConvert.DeserializeObject<List<Bairro>>(json);
-                    }
-                    else
-                        throw new Exception(response.RequestMessage.Content.ToString());
-
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    arr_bairros = JsonConvert.DeserializeObject<List<Bairro>>(json);
                 }
-                return arr_bairros;
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+
             }
+            return arr_bairros;
+        }
+
+        public static async Task<List<Cidade>> GetCidadesByEstado(string uf)
+        {
+            List<Cidade> arr_cidade = new List<Cidade>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/cidade/by-uf?=");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    arr_cidade = JsonConvert.DeserializeObject<List<Cidade>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+            return arr_cidade;
+
         }
     }
+}
 
 
--- AppCep/AppCep/View/BairrosPorCidade.xaml.cs
+++ AppCep/AppCep/View/BairrosPorCidade.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppCep.Model;
+using AppCep.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,37 @@ namespace AppCep.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairrosPorCidade : ContentPage
     {
+        ObservableCollection<Cidade> Lista_cidade = new ObservableCollection<Cidade>();
+
+        ObservableCollection<Bairro> Lista_bairro = new ObservableCollection<Bairro>();
+
         public BairrosPorCidade()
         {
             InitializeComponent();
+
+            pck_estado.ItemsSource = Lista_cidade;
+
+            lst_bairros.ItemsSource = Lista_bairro;
+        }
+
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = sender as Picker;
+
+                string estado_selecionado = disparador.SelectedItem as string;
+
+                List<Cidade> arr_cidades = await DataService.GetCidadesByEstado(estado_selecionado);
+
+                Lista_cidade.Clear();
+
+                arr_cidades.ForEach(i => Lista_cidade.Add(i));
+            }
+            catch (Exception ex) 
+            {
+                await DisplayAlert ("OPS", ex.Message, "ok");
+            }
         }
     }
 }
