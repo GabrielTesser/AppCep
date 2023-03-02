fdfb342626b768a8b25d563665ea6778a822a7bf
Usuario <Usuario@CPD02-M13>
2023-03-02 15:19:09 -0300

    sla

--- AppCep/AppCep/Service/DataService.cs
+++ AppCep/AppCep/Service/DataService.cs
@@ -32,7 +32,7 @@ namespace AppCep.Service
             return end;
         }
 
-        public static async Task<List<Bairro>> GetBarrosByCidade(int id_cidade)
+        public static async Task<List<Bairro>> GetBairrosByCidade(int id_cidade)
         {
             List<Bairro> arr_bairros = new List<Bairro>();
 
@@ -71,7 +71,48 @@ namespace AppCep.Service
             return arr_cidade;
 
         }
+
+        public static async Task<List<Logradouro>> GetLogradourosByBairrosAndIdCidade(int id_cidade, string bairro)
+        {
+            List<Logradouro> arr_logradouro = new List<Logradouro>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://cep.metoda.com.br/logradouro/by-bairro?id_cidade={id_cidade}&bairro={bairro}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_logradouro = JsonConvert.DeserializeObject<List<Logradouro>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+            return arr_logradouro;
+        }
+
+        public static async Task<List<Cep>> GetCepByLogradouro(int id_cidade, string bairro)
+        {
+            List<Cep> arr_cep = new List<Cep>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://cep.metoda.com.br/cep/by-logradouro?logradouro={bairro}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_cep = JsonConvert.DeserializeObject<List<Cep>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+            return arr_cep;
+
+
+        }
     }
 }
-
 
